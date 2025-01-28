ad99bbec2fdfdf2e71dfe9dac1b60720fb39d1ab
Tanya Romanovskaia <tvromanovskaia@gmail.com>
2025-01-28 09:16:48 +0300

    added open method

---
+++ src/test/java/FormTests.java
@@ -0,0 +1,29 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class FormTests {
+    @Test
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
+        Configuration.timeout = 5000; // default 4000
+    }
+
+    void formTests () {
+
+        open("//automation-practice-form");
+        $("#userName").setValue("Alex");
+        $("#userEmail").setValue("alex@egorov.com");
+        $("#currentAddress").setValue("Some street 1");
+        $("#permanentAddress").setValue("Another street 1");
+        $("#submit").click();
+
+    }
+}
